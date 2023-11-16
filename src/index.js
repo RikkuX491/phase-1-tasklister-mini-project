658a7305b1411fd25ff88324b35a628355848d68
Ricardo Guerra <RikkuG491@gmail.com>
2023-11-16 15:38:44 -0500

    Added solution code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const tasksUlElement = document.getElementById('tasks')
+
+  const createTaskForm = document.getElementById('create-task-form')
+  createTaskForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const inputTaskElement = document.getElementById('new-task-description')
+
+    const liElement = document.createElement('li')
+    liElement.textContent = inputTaskElement.value + " "
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = "X"
+    deleteButton.setAttribute('data-description', inputTaskElement.value)
+    deleteButton.addEventListener('click', deleteTask)
+
+    liElement.appendChild(deleteButton)
+
+    tasksUlElement.appendChild(liElement)
+
+    createTaskForm.reset()
+  })
 });
+
+function deleteTask(e){
+  e.target.parentNode.remove()
+}
